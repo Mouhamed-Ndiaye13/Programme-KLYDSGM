b15a02821f7b99fd39842d2a436c2654c9c2040a
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2025-12-07 09:45:03 -0800

    programme djamil

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Date cible du prochain programme
-const eventDate = new Date("nov 28, 2025 17:00:00").getTime();
+const eventDate = new Date("jan 02, 2026 17:00:00").getTime();
 
 const countdownEl = document.getElementById("countdown");
 
